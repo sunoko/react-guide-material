8f4d6c853955dc8228aaf93a2acaebe0ee270b79
KazukiChinone <staybesidehangon@gmail.com>
2024-02-27 07:26:42 +0900

    props

--- 04_react_basic/src/090_props/start/Example.js
+++ 04_react_basic/src/090_props/start/Example.js
@@ -1,5 +1,11 @@
 import Child from "./components/Child";
 
-const Example = () => <Child />;
+const Example = () => (
+  <>
+    <Child color="blue"/>
+    <Child color="red"/>
+    <Child />
+  </>
+)
 
 export default Example;
